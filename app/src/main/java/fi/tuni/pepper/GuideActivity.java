1159cbf24b7022543af5e4c33d6227d2ba1c37b4
U-DESKTOP-FH1PMIH\Leksi <aleksi.hyttinen@tuni.fi>
2022-12-12 13:02:23 +0200

    Add GuideActivity

---
+++ app/src/main/java/fi/tuni/pepper/GuideActivity.java
@@ -0,0 +1,71 @@
+package fi.tuni.pepper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.aldebaran.qi.sdk.QiContext;
+import com.aldebaran.qi.sdk.QiSDK;
+import com.aldebaran.qi.sdk.RobotLifecycleCallbacks;
+import com.aldebaran.qi.sdk.builder.SayBuilder;
+import com.aldebaran.qi.sdk.design.activity.RobotActivity;
+import com.aldebaran.qi.sdk.object.conversation.BodyLanguageOption;
+import com.aldebaran.qi.sdk.object.conversation.Say;
+
+import org.w3c.dom.Text;
+
+public class GuideActivity extends RobotActivity implements RobotLifecycleCallbacks {
+    private LinearLayout guide_texts;
+    private String text = "";
+    private View read_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+        QiSDK.register(this, this);
+        View back = findViewById(R.id.back);
+        read_btn = findViewById(R.id.read);
+        back.setOnClickListener(view -> {
+            Intent intent = new Intent(view.getContext(), GameActivity.class);
+            view.getContext().startActivity(intent);
+        });
+        guide_texts = findViewById(R.id.guide_texts);
+        StringBuilder output = new StringBuilder();
+        for(int i = 0; i < guide_texts.getChildCount(); i++) {
+            TextView child = (TextView) guide_texts.getChildAt(i);
+            output.append(child.getText().toString());
+            output.append(" ");
+        }
+        text = output.toString();
+    }
+
+    @Override
+    public void onRobotFocusGained(QiContext qiContext) {
+        System.out.println("focus");
+        read_btn.setOnClickListener((view) -> new Thread(() -> {
+            runOnUiThread(()-> read_btn.setEnabled(false));
+            System.out.println(text);
+            Say say = SayBuilder.with(qiContext)
+                    .withText(text)
+                    .withBodyLanguageOption(BodyLanguageOption.DISABLED)
+                    .build();
+            say.async().run();
+        }).start());
+        runOnUiThread(()->read_btn.setEnabled(true));
+
+    }
+
+    @Override
+    public void onRobotFocusLost() {
+
+    }
+
+    @Override
+    public void onRobotFocusRefused(String reason) {
+
+    }
+}
